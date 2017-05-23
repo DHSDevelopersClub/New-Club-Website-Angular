db44fdf8395bf4646fb421bb6c1b0a71fe2586ac
Max Starr <max.starr@techie.com>
2017-05-23 10:13:01 -0700

    Replaced Placeholder Image and Slight Formatting

--- src/app/roster/roster.component.ts
+++ src/app/roster/roster.component.ts
@@ -11,17 +11,17 @@ export class RosterComponent implements OnInit {
     {
       name: 'Max Starr',
       description: 'Is a manss',
-      imgurl: 'http://placeholder.com/image.png'
+      imgurl: 'https://3.bp.blogspot.com/-W__wiaHUjwI/Vt3Grd8df0I/AAAAAAAAA78/7xqUNj8ujtY/s1600/image02.png'
     },
     {
       name: 'Leonid Krasnov',
       description: 'What a lad',
-      imgurl: 'http://placeholder.com/image.png'
+      imgurl: 'http://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg'
     },
     {
       name: 'D-Grubu',
       description: 'Description',
-      imgurl: 'http://placeholder.com/image.png'
+      imgurl: 'https://beebom-redkapmedia.netdna-ssl.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg'
     }
   ]
 
